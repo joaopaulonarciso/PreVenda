de5531e4484d86aa1b7e7b61444aa66882f5d851
Joao Paulo Narciso <joaopaulonarciso@sommus.com>
2017-04-21 11:40:29 -0300

    Iniciando novamente o projeto, criado a Entidade Cliente

---
+++ Domain/PreVenda.Domain/Entities/Cliente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreVenda.Domain.Entities
+{
+    public class Cliente
+    {
+        string _nome = string.Empty;
+    string _endereco = string.Empty;
+
+    public int ClienteId { get; set; }
+    public string Nome
+    {
+        get
+        {
+            return _nome ?? string.Empty;
+        }
+        set
+        {
+            _nome = value;
+        }
+    }
+    public string Endereco
+    {
+        get
+        {
+            return _endereco ?? string.Empty;
+        }
+        set
+        {
+            _endereco = value;
+        }
+    }
+}
+}
